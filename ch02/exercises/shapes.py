ac9fed3c7e68340752f58fd5d995a85a7b673e7c
sachinjojode <sachinjojode@gmail.com>
2023-02-13 17:22:33 -0500

    shapes.py

--- ch02/exercises/shapes.py
+++ ch02/exercises/shapes.py
@@ -1,3 +1,15 @@
 import pygame
 
-screen = pygame.display.set_mode()
+pygame.init()
+screen = pygame.display.set_mode((600,600))
+screen.fill("black")
+
+pygame.draw.circle(screen, "white", (300,400), 150)
+pygame.display.flip()
+pygame.draw.circle(screen, "white", (300,225), 100)
+pygame.display.flip()
+pygame.draw.circle(screen, "white", (300,100), 50)
+pygame.display.flip()
+
+#pygame.display.update()
+pygame.time.wait(1000)
