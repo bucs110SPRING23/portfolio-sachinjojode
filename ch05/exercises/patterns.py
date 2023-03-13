4a3fe696dd08251668e69815dea93565146313d1
sachinjojode <sachinjojode@gmail.com>
2023-03-13 18:04:23 -0400

    scope.py

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,15 +1,12 @@
 def star_pyramid():
     rows = int(input("Please enter a number: "))
-    for i in range(rows + 1):
+    for i in range(1, rows + 1):
         print("*" * i)
 
 def rstar_pyramid():
     rows = int(input("Please enter a number: "))
-    rows1 = rows
-    print("")
-    for i in range(rows + 1):
-        print("*" * rows1)
-        rows1 -= 1
+    for i in range(rows, 0 , -1):
+        print("*" * i)
     
 star_pyramid()
 rstar_pyramid()
