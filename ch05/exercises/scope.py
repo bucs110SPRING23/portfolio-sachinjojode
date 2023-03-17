0a08f25f13df4f3f805369bc44506d169419f910
sachinjojode <sachinjojode@gmail.com>
2023-03-17 12:12:54 -0400

    Readme

--- ch05/exercises/scope.py
+++ ch05/exercises/scope.py
@@ -5,17 +5,20 @@ def multiply_without_multiplication(num1,num2):
     return num3
 
 def exponent_without_exponentation(num,exp):
-    num3 = 1
-    for i in range(0,exp):
-        num3 *= num
-    return num3
+    return num**exp
 
 def square(number):
     return multiply_without_multiplication(number,number)
 
-result1 = multiply_without_multiplication(5,8)
-print(result1)
-result2 = exponent_without_exponentation(2,7)
-print(result2)
-result3 = square(6)
-print(result3)
+def main():
+    x = int(input("Enter a number: "))
+    y = int(input("Enter a number: "))
+    result1 = multiply_without_multiplication(x,y)
+    print(result1)
+    result2 = exponent_without_exponentation(x,y)
+    print(result2)
+    result3 = square(x)
+    print(result3)
+    
+    
+main()
