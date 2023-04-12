bff9cd077fd73a712615e45c4fe4cc28f6c77c8c
YoheiHayamizu <yhayami1@binghamton.edu>
2023-04-12 14:04:57 -0400

    Update main.py

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -17,10 +17,27 @@ def threenp1range(upper_limit):
         objs_in_sequence[i] = count
     return objs_in_sequence
 
-def main():
-    print(threenp1range(20))
-    
-main()
+def graph_coordinates(threenplus1_iters_dict, window):
+    coordinates = list(threenplus1_iters_dict.items())
+    print(coordinates)
+    max_count = max(coordinates, key=lambda x: x[1])
+    pygame.draw.lines(window, "blue", False, coordinates)
+    width, height = window.get_size()
+    new_display = pygame.transform.scale(window, (width * 5, height * 5))
+    window.blit(new_display, (0,0))
+    new_display = pygame.transform.flip(window, False, True)
+    window.blit(new_display, (0,0))
+    font = pygame.font.Font(None, 42)
+    msg = font.render("Max Count: " + str(max_count[0]), False, "white", "black")
+    window.blit(msg, (10, 10))
+    pygame.display.flip()
 
-def graph_coordinates(threenplus1_iters_dict):
-    print(1)
+def main():
+    pygame.init()
+    window = pygame.display.set_mode()
+    window.fill("white")
+    pygame.display.flip()
+    graph_coordinates(threenp1range(20), window)
+    pygame.time.wait(10000)
+
+main()
