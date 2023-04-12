e3b191bb78cf755960a4755a5abdc26c07d75c6e
sachinjojode <sachinjojode@gmail.com>
2023-04-12 13:20:54 -0400

    cipher pt.1

--- ch06/exercises/cipher.py
+++ ch06/exercises/cipher.py
@@ -1,3 +1,6 @@
+import json
+
+
 def caesar_cipher(text, shift):
     """
     Encrypts or decrypts a message using the Caesar cipher technique.
@@ -14,15 +17,24 @@ def caesar_cipher(text, shift):
     """
 
     result = ""
-    for char in text:
-        if char.isalpha():
-            # Determine the case of the character
+    for i, char in enumerate(text):
+        if char.isalpha() and i % 2 != 0:
+        # Determine the case of the character
             start = ord('A') if char.isupper() else ord('a')
-            # Calculate the new position of the character after the shift
+        # Calculate the new position of the character after the shift
             new_pos = (ord(char) - start + shift) % 26
-            # Convert the new position back to a character
+        # Convert the new position back to a character
             char = chr(start + new_pos)
         result += char
     return result
 
-print(caesar_cipher("Hello World", 0))
+def main():
+    file_pointer = open("encrypted.txt","w")
+    result = caesar_cipher("abcdefg", 3)
+    file_pointer.write(result)
+    file_pointer.close()
+    
+main()
+
+
+print()
